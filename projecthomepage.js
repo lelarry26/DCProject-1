ec97cb1e9e11f592b093e4711f8e4bfdbece9d47
Derek Szeto <szeto.d71@gmail.com>
2023-07-22 18:22:16 -0400

    added cards and working on getting info from GS to cards

--- projecthomepage.js
+++ projecthomepage.js
@@ -1,12 +1,11 @@
 const form = document.getElementById("gameSearch");
 const gameData = document.getElementById("gameData");
 
+
 const gameSpotAPI = "42ca3a8ff2dc770f2e69d2367678840e5faef0ce"
+const giantBombAPI = "17243f34422c1b6b140df1f00cfa3ad42fc7af79"
 // const rawgAPi = "ff0ae1ba7da843b6af414828bfa56f91"
-form.addEventListener('submit', (event) => {
-event.preventDefault();
-console.log(event);
-const gameSearch = event.target.gameSearch.value;
+
 // const date = new Date();
 // let tmrwYear = date.getYear().toString().substr(-2);
 // let tmrwMonth = String(date.getMonth() + 1).padStart(2, "0");
@@ -17,11 +16,86 @@ const gameSearch = event.target.gameSearch.value;
 
 
 //below fetch will use search box and pull all games matching that search query in data
-  fetch(`https://cors-proxy.fringe.zone/https://www.gamespot.com/api/games/?format=json&filter=name:${gameSearch}&api_key=${gameSpotAPI}`)
-  .then(res => res.json())
-  .then(data => {
+  // fetch(`https://cors-proxy.fringe.zone/https://www.gamespot.com/api/games/?format=json&filter=name:${gameSearch}&api_key=${gameSpotAPI}`)
+  // fetch(`https://cors-proxy.fringe.zone/https://www.gamespot.com/api/games/?format=json&filter=name:${gameSearch}&api_key=${gameSpotAPI}`)
+  // .then(res => res.json())
+  // .then(data => {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      console.log(event);
+      const gameSearch = event.target.gameSearch.value;
+      fetch(`https://cors-proxy.fringe.zone/https://www.gamespot.com/api/games/?format=json&filter=name:${gameSearch}&api_key=${gameSpotAPI}`)
+      .then(res => res.json())
+      .then(data => {
     console.log(data)
-    // for(i=0; i<data.results.length; i++){
+    data.results.map((datum) => {
+          const gameListItem = document.createElement("data");
+          const gameList = document.getElementById("gameList");
+          gameList.appendChild(gameListItem);
+          gameListItem.innerHTML = `
+          <div class="card">
+          <div id = "gameImage">
+          <img src="${datum.image.original}" width = "200" height = "150"></div>
+          <div id="card-body">
+            <h5 id="gameName">${datum.name}</h5>
+            <p id="gameDescription">${datum.deck}</p>
+          </div>
+          <ul id="list-group list-group-flush">
+            <li id="gameSite">${datum.site_detail_url}</li>
+            <li id="gameGenre">${datum.genres.name}</li>
+          </ul>
+          <div id="card-body">
+            <a href="#" id="card-link1"></a>
+            <a href="#" id="card-link2"></a>
+          </div>
+        </div>
+        `
+    })
+  })
+})
+//reached max
+  //   data.results.map((datum) => {
+  //     const gameListItem = document.createElement("data");
+  //     const gameList = document.getElementById("gameList");
+  //     gameList.appendChild(gameListItem);
+  //     gameListItem.innerHTML = `
+  //     <div class="card">
+  //     <div id = "gameImage">
+  //     <img src="${datum.image}" width = "200" height = "150"></div>
+  //     <div id="card-body">
+  //       <h5 id="gameName">${datum.name}</h5>
+  //       <p id="gameDescription">${datum.description}</p>
+  //     </div>
+  //     <ul id="list-group list-group-flush">
+  //       <li id="gameSite">${datum.site_detail_url}</li>
+  //       <li id="gameGenre">${datum.number_of_user_reviews}</li>
+  //     </ul>
+  //     <div id="card-body">
+  //       <a href="#" id="card-link1"></a>
+  //       <a href="#" id="card-link2"></a>
+  //     </div>
+  //   </div>
+  //     `
+  //   });
+    // fetch(`https://cors-proxy.fringe.zone/https://www.gamespot.com/api/videos/?filter=association:5000-${data.results.id}?format=json&api_key=${gameSpotAPI}`)
+    //  .then(res => res.json())
+    // .then(data => {
+    // console.log(data)
+    // data.results.map((datum) => {
+    //   console.log(datum);
+    // })
+    //   })
+    //   fetch(`https://cors-proxy.fringe.zone/https://www.gamespot.com/api/videos/?filter=association:5000-${data.results.id}&api_key=${gameSpotAPI}`)
+    //   .then(res => res.json())
+    //    .then(data => {
+    //  console.log(data)
+    //  data.results.map((datum) => {
+    //    console.log(datum);
+    //  })
+    //    });
+
+  
+        // for(i=0; i<data.results.length; i++){
     //   const gameListItem = document.createElement("button");
     //   const gameList = document.getElementById("gameList");
     //   gameList.appendChild(gameListItem);
@@ -32,60 +106,10 @@ const gameSearch = event.target.gameSearch.value;
     //       TEST
     //       <div id="dataresult">${data.results[i].release_date}</div>
     //   `
-    data.results.map((datum) => {
-      const gameListItem = document.createElement("data");
-      const gameList = document.getElementById("gameList");
-      gameList.appendChild(gameListItem);
-      gameListItem.innerHTML = `
-       <div class="container">
-            <p class="containerP">${datum.name}</p>
-        </div>
-
-        <div id="someid" class="container2">
-            <p>Test</p>
-            <p>Test</p>
-            <p>Test</p>
-        </div>
-  
-      `
-    })
     // if (`${datum.image.original}` === null) {
     //     document.getElementsByTagName("img").innerHTML = "<img src='https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-1.jpg'>"
     //   }
-  })
 
-});
-
-let showVideoEmbed = (embed, msg, element) => { 
-  let video = document.createElement("video");
-
-  let newHeight = embed.video.height < 300 ? embed.video.height : 400;
-
-  video.style.height = newHeight + 'px';
-  video.style.width = 'auto';
-
-  let url = embed.video.proxyURL ? embed.video.url : embed.video.url;
-  video.src = url;
-  video.classList.add("previewImage");
-  
-  video.onmouseenter = e => {
-    video.setAttribute('controls', 'true');
-  }
-
-  video.onmouseleave = e => {
-    video.removeAttribute('controls');
-  }
-  
-  video.onclick = e => {
-    video.paused ? video.play() : video.pause();
-  }
-
-  video.onended = e => {
-    video.currentTime = 0;
-  }
-
-  element.appendChild(video);
-}
       // ${datum.name}
       //   <img src="${datum.image.original}" width = "200" height = "150">
       //   <h1>${datum.name}</h1>
