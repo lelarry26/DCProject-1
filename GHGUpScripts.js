d0b86f755dd8bbb7098aa9c1d1dd0a636460eac1
Larry Le <larrygsu@gmail.com>
2023-07-25 20:24:29 -0400

    Updating cleaner files and code

---
+++ GHGUpScripts.js
@@ -0,0 +1,48 @@
+const form = document.getElementById("gameSearch");
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const giantBombAPI = "4ccf8f3bad38d69d7a94f106fe749bdff1d91ef5";
+  const date = new Date();
+  let tmrwYear = date.getYear().toString().substr(-2);
+  let tmrwMonth = String(date.getMonth() + 1).padStart(2, "0");
+  let tmrwDay = date.getDate() + 1;
+  let tmrwDate = `${tmrwYear}${tmrwMonth}${tmrwDay}`;
+
+  // Initial Giantbomb Search
+   fetch(`https://cors-proxy.fringe.zone/https://www.giantbomb.com/api/releases/?format=json&?&filter=release_date:${tmrwDate}|330701&sort=release_date:asc&api_key=${giantBombAPI}`)
+   .then(res => res.json())
+   .then(data => {
+      console.log(data)
+
+      //Image
+      const gameImages = document.querySelectorAll(".gameImage");
+      gameImages.forEach((element, i) => {
+        element.innerHTML = `<img src="${data.results[i].image.small_url}" width="200" height="200">`;
+      });
+
+      //Names
+      const gameNames = document.querySelectorAll(".gameName");
+      gameNames.forEach((element, i) => {
+        element.innerHTML = `${data.results[i].name}`;
+      });
+
+      //Blurb
+      const gameBlurbs = document.querySelectorAll(".gameBlurb");
+      gameBlurbs.forEach((element, i) => {
+        element.innerHTML = `${data.results[i].description}`;
+      });
+        
+      //Release Date
+      const gameReleases = document.querySelectorAll(".gameRelease");
+      gameReleases.forEach((element, i) => {
+        element.innerHTML = `Releases on ${data.results[i].expected_release_month}-${data.results[i].expected_release_day}-${data.results[i].expected_release_year}`;
+      });
+
+      //Game URL
+      const gameURL = document.querySelectorAll(".gameURL");
+      gameReleases.forEach((element, i) => {
+        element.innerHTML = `Check out - <a href="${data.results[i].site_detail_url}">here</a>`
+      });
+
+})
+})
